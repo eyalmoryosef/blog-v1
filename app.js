6cd5182522b1b5f24d7d6ffe62c7736ba62e7d5a
eyal <eyaloosh110@gmail.com>
2022-06-26 01:18:50 +0300

    complete challange 17 . added more functionality to website and added more routes

--- app.js
+++ app.js
@@ -9,17 +9,54 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+const posts = [];
 
 app.set('view engine', 'ejs');
-
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
 
 
-
-
-
+app.get('/', function(req, res) {
+  res.render("home", {
+    startingContent: homeStartingContent,
+    daysContent: posts
+  });
+});
+app.get('/contact', function(req, res) {
+  res.render("contact", {
+    contactContent: contactContent
+  });
+});
+app.get('/about', function(req, res) {
+  res.render("about", {
+    aboutContent: aboutContent
+  });
+});
+app.get('/compose', function(req, res) {
+  res.render("compose")
+});
+app.post('/compose', function(req, res) {
+  const post = {
+    title: req.body.composeTitle,
+    content: req.body.composeText
+  };
+  posts.push(post);
+  res.redirect('/');
+});
+app.get('/posts/:postName', function(req, res) {
+  const postTitle = req.params.postName;
+  console.log(postTitle);
+  console.log(posts);
+  posts.forEach((item) => {
+    console.log(item);
+    if (item.title === postTitle) {
+      console.log("found!");
+    }
+  });
+});
 
 
 
